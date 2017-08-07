cbb09ac912ea0f35e5feac9700c58a796b402fb9
samuelloveyou <samuelloveyou@gmail.com>
2017-08-07 04:22:45 +0300

    one

--- demonstration.js
+++ demonstration.js
@@ -1,3 +1,12 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = 'welcome to: "share a bike"';
 }
+
+function yourFunction() {
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
