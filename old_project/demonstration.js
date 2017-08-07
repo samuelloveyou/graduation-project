9cbdc70fac2b873cde18f77335102bcfb0608788
samuelloveyou <samuelloveyou@gmail.com>
2017-08-07 12:09:45 +0300

    Project with nadav

---
+++ old_project/demonstration.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = 'welcome to: "share a bike"';
+}
+
+function yourFunction() {
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ourFunction() {
+    var x = document.getElementById('my');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
